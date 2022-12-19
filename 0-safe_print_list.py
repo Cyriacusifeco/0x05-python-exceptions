b566ad945d827afe9c687d766793e951bb718c78
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-19 23:59:04 +0000

    print list

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     i = 0
     count = 0
@@ -9,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             count = count + 1
 
-        except:
+        except IndexError:
             return count
 
         i = i + 1
